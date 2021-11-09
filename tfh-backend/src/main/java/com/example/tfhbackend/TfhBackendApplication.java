b6f857ec5d7c95ad272fb2e628a6ad6f42810214
ianic1999 <ianicumanschii@gmail.com>
2021-11-09 22:14:29 +0200

    [tfh-21] - add default admin user

--- tfh-backend/src/main/java/com/example/tfhbackend/TfhBackendApplication.java
+++ tfh-backend/src/main/java/com/example/tfhbackend/TfhBackendApplication.java
@@ -1,13 +1,47 @@
 package com.example.tfhbackend;
 
+import com.example.tfhbackend.model.User;
+import com.example.tfhbackend.model.enums.UserRole;
+import com.example.tfhbackend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class TfhBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TfhBackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(UserRepository userRepository,
+                                  BCryptPasswordEncoder passwordEncoder) {
+        return args -> {
+            addDefaultAdminUser(userRepository, passwordEncoder);
+        };
+    }
+
+    private void addDefaultAdminUser(UserRepository userRepository,
+                                     BCryptPasswordEncoder passwordEncoder) {
+        Optional<User> optionalUser = userRepository.findByPhone("+37369999999");
+        if (optionalUser.isEmpty()) {
+            User user = User.builder()
+                    .firstName("ADMIN")
+                    .lastName("ADMIN")
+                    .phone("+37369999999")
+                    .email("admin@gmail.com")
+                    .password(passwordEncoder.encode("password"))
+                    .role(UserRole.ADMIN)
+                    .confirmed(true)
+                    .build();
+            userRepository.save(user);
+        }
+    }
 }
