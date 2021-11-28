08bca8e4f2d9f8447216fab771667da51703cf06
ianic1999 <ianicumanschii@gmail.com>
2021-11-28 10:54:17 +0200

    create order dto

---
+++ tfh-backend/src/main/java/com/example/tfhbackend/dto/OrderDTO.java
@@ -0,0 +1,14 @@
+package com.example.tfhbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class OrderDTO {
+    private Long id;
+}
